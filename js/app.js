0537dd2a43b97d77114296dc6d2888927ee2ff9d
Vikas Lalwani <lalwani.vikas20@gmail.com>
2015-01-18 23:29:23 +0530

    Transferring files from desktop to GitHub.

---
+++ js/app.js
@@ -0,0 +1,65 @@
+var color = $(".selected").css("background-color");
+var $canvas = $("canvas")
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+
+//when clicking on colors
+$(".controls").on("click", "li", function(){
+	$(this).siblings().removeClass("selected");
+	$(this).addClass("selected");
+	color = $(this).css("background-color");
+});
+
+//When new color is pressed
+$("#revealColorSelect").click(function(){
+	changeColor();
+	$("#colorSelect").toggle();
+});
+
+//updating the color
+function changeColor(){
+	var r = $("#red").val();
+	var g = $("#blue").val();
+	var b = $("#green").val();
+
+	$("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+}
+
+$("input[type=range]").change(changeColor);
+
+$("#addNewColor").click(function(){
+	var $newColor = $("<li></li>");
+	$newColor.css("background-color", $("#newColor").css("background-color"));
+
+	$(".controls ul").append($newColor);
+
+	$newColor.click();
+});
+
+$canvas.mousedown(function(e){
+	lastEvent = e;
+	mouseDown = true;
+}).mousemove(function(e){
+	if(mouseDown){
+		context.beginPath();
+		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		context.lineTo(e.offsetX, e.offsetY);
+		context.strokeStyle = color;
+		context.stroke();
+		lastEvent = e;
+	}
+}).mouseup(function(){
+	mouseDown = false;
+}).mouseleave(function(){
+	$canvas.mouseup();
+});
+
+
+
+
+
+
+
+
+
